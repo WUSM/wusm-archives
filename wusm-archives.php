8a2970e15201fe4714e1f5bcd2e2934de6af3b6f
Aaron Graham <aegraham@gmail.com>
2014-05-22 16:37:15 -0500

    add sticky posts to the first page

--- wusm-archives.php
+++ wusm-archives.php
@@ -81,14 +81,42 @@ class wusm_archives_plugin {
 
 	private function load_wusm_posts( $type, $page ) {
 		$output = "";
-		
+
+		if( $page === 1 ) {
+			// WP_Query arguments
+			$args = array (
+				'post_type' 	 => $type,
+				'posts_per_page' => '30',
+				'paged'     	 => $page,
+				'orderby' 	     => 'date',
+				'meta_query' => array(
+					array(
+						'key' => 'sticky',
+						'value' => 1,
+						'compare' => '=',
+					)
+				)
+			);
+
+			// The Query
+			$query = new WP_Query( $args );
+
+			if ( $query->have_posts() ) {
+				while ( $query->have_posts() ) {
+					$query->the_post();
+					$post_id = get_the_ID();
+					$output .= $this->print_the_wusm_post($type, $post_id);
+				}
+			}
+			$output .= "<hr>";
+		}
+
 		// WP_Query arguments
 		$args = array (
 			'post_type' 	 => $type,
 			'posts_per_page' => '30',
 			'paged'     	 => $page,
-			'orderby' 	     => 'menu_order',
-			'order'          => 'ASC'
+			'orderby' 	     => 'date'
 		);
 
 		// The Query
@@ -97,57 +125,9 @@ class wusm_archives_plugin {
 		// The Loop
 		if ( $query->have_posts() ) {
 			while ( $query->have_posts() ) {
-				add_filter( 'excerpt_more', function() { return ''; } );
-
 				$query->the_post();
 				$post_id = get_the_ID();
-				$title_text = apply_filters( "{$type}_title_text", '<h3>' . get_the_title() . '</h3>', $post_id );
-				$date_text = apply_filters( "{$type}_date_text", get_the_date("m/d/y"), $post_id );
-				$link_text = apply_filters( "{$type}_link_text", $title_text, $post_id );
-				$excerpt_text = apply_filters( "{$type}_excerpt_text", get_the_excerpt(), $post_id );
-				$thumbnail_size = apply_filters( "{$type}_thumbnail_size", 'post-thumbnail', $post_id );
-				$link_field = apply_filters( "{$type}_link_field", '', $post_id );
-				
-				$target = "";
-				
-				if( $link_field !== '' ) {
-					$link = get_field( $link_field );
-					if( is_array( $link ) ) {
-						$target = ( $link['new_window'] ) ? " target='_blank'" : "";
-						$url = $link['url'];
-					} else {
-						$url = $link;
-					}
-				} else {
-					$url = get_permalink( $post_id );
-				}
-				if( $url === 'http://' )
-					$url = 'javascript:return false;';
-				
-				//a hack for now
-				if($type != 'in_focus') {
-					$output .= "<div class='$type-custom-archive-entry custom-archive-entry clearfix'>";
-					$output .= ( $date_text === "" ) ? "" : "<span class='$type-custom-archive-date custom-archive-date'>$date_text</span><br>";
-					$output .= ( $link_text === "" ) ? "" : "<span class='$type-custom-archive-link custom-archive-link'><a$target href='$url'>$link_text</a></span>";
-					$output .= ( $excerpt_text === "" ) ? "" : "<span class='$type-custom-archive-excerpt custom-archive-excerpt'>$excerpt_text</span><br>";
-					$output .= "<a$target href='$url'>";
-					$output .= get_the_post_thumbnail($post_id, $thumbnail_size, array( 'class'	=> "$type-custom-archive-thumb custom-archive-thumb" ) );
-					$output .= "</a>";
-					$output .= "</div>";
-				} else {
-					$output .= "<div class='$type-custom-archive-entry custom-archive-entry clearfix'>";
-					$output .= "<div style='float: right; width: 330px; padding: 20px;'>";
-					$output .= ( $title_text === "" ) ? "" : "<span class='$type-custom-archive-title custom-archive-title'>$title_text</span>";
-					$output .= "<hr>";
-					$output .= ( $date_text === "" ) ? "" : "<span class='$type-custom-archive-date custom-archive-date'>$date_text</span><br>";
-					$output .= ( $excerpt_text === "" ) ? "" : "<span class='$type-custom-archive-excerpt custom-archive-excerpt'>$excerpt_text</span>";
-					$output .= ( $link_text === "" ) ? "" : "<span class='$type-custom-archive-link custom-archive-link'><a$target href='$url'>$link_text</a></span>";
-					$output .= "</div>";
-					$output .= "<a$target href='$url'>";
-					$output .= get_the_post_thumbnail($post_id, $thumbnail_size, array( 'class'	=> "$type-custom-archive-thumb custom-archive-thumb" ) );
-					$output .= "</a>";
-					$output .= "</div>";
-				}
+				$output .= $this->print_the_wusm_post($type, $post_id);
 			}
 		} else {
 			$output = "false";
@@ -156,6 +136,60 @@ class wusm_archives_plugin {
 		// Restore original Post Data
 		wp_reset_postdata();
 		
+		return $output;
+	}
+
+	private function print_the_wusm_post($type, $post_id) {
+		$output = '';
+		add_filter( 'excerpt_more', function() { return ''; } );
+
+		$title_text = apply_filters( "{$type}_title_text", '<h3>' . get_the_title() . '</h3>', $post_id );
+		$date_text = apply_filters( "{$type}_date_text", get_the_date("m/d/y"), $post_id );
+		$link_text = apply_filters( "{$type}_link_text", $title_text, $post_id );
+		$excerpt_text = apply_filters( "{$type}_excerpt_text", get_the_excerpt(), $post_id );
+		$thumbnail_size = apply_filters( "{$type}_thumbnail_size", 'post-thumbnail', $post_id );
+		$link_field = apply_filters( "{$type}_link_field", '', $post_id );
+		
+		$target = "";
+		
+		if( $link_field !== '' ) {
+			$link = get_field( $link_field );
+			if( is_array( $link ) ) {
+				$target = ( $link['new_window'] ) ? " target='_blank'" : "";
+				$url = $link['url'];
+			} else {
+				$url = $link;
+			}
+		} else {
+			$url = get_permalink( $post_id );
+		}
+		if( $url === 'http://' )
+			$url = 'javascript:return false;';
+		
+		//a hack for now
+		if($type != 'in_focus') {
+			$output .= "<div class='$type-custom-archive-entry custom-archive-entry clearfix'>";
+			$output .= ( $date_text === "" ) ? "" : "<span class='$type-custom-archive-date custom-archive-date'>$date_text</span><br>";
+			$output .= ( $link_text === "" ) ? "" : "<span class='$type-custom-archive-link custom-archive-link'><a$target href='$url'>$link_text</a></span>";
+			$output .= ( $excerpt_text === "" ) ? "" : "<span class='$type-custom-archive-excerpt custom-archive-excerpt'>$excerpt_text</span><br>";
+			$output .= "<a$target href='$url'>";
+			$output .= get_the_post_thumbnail($post_id, $thumbnail_size, array( 'class'	=> "$type-custom-archive-thumb custom-archive-thumb" ) );
+			$output .= "</a>";
+			$output .= "</div>";
+		} else {
+			$output .= "<div class='$type-custom-archive-entry custom-archive-entry clearfix'>";
+			$output .= "<div style='float: right; width: 330px; padding: 20px;'>";
+			$output .= ( $title_text === "" ) ? "" : "<span class='$type-custom-archive-title custom-archive-title'>$title_text</span>";
+			$output .= "<hr>";
+			$output .= ( $date_text === "" ) ? "" : "<span class='$type-custom-archive-date custom-archive-date'>$date_text</span><br>";
+			$output .= ( $excerpt_text === "" ) ? "" : "<span class='$type-custom-archive-excerpt custom-archive-excerpt'>$excerpt_text</span>";
+			$output .= ( $link_text === "" ) ? "" : "<span class='$type-custom-archive-link custom-archive-link'><a$target href='$url'>$link_text</a></span>";
+			$output .= "</div>";
+			$output .= "<a$target href='$url'>";
+			$output .= get_the_post_thumbnail($post_id, $thumbnail_size, array( 'class'	=> "$type-custom-archive-thumb custom-archive-thumb" ) );
+			$output .= "</a>";
+			$output .= "</div>";
+		}
 		return $output;
 	}
 }
